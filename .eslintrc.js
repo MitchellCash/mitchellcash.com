3ae09ff58bf264f488d91a29b75a1b3e9944ea17
Mitchell Cash <mitchell@mitchellcash.com>
2019-02-21 21:42:17 +1000

    Add Travis CI support

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
 
     // options to emulate prettier setup
     semi: ["error", "never"],
-    "max-len": ["error", { code: 80 }],
+    "max-len": ["warn", { code: 80 }],
     "template-curly-spacing": ["error", "always"],
     "arrow-parens": ["error", "as-needed"],
 
