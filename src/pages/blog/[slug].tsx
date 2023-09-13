6c8296f5e259a03b3109d9412e9d2d4a8193685c
Mitchell Cash <me@mitchellcash.com>
2023-09-13 11:20:13 +0100

    Use ReactMarkdown to avoid dangerouslySetInnerHTML

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -3,8 +3,7 @@ import { getPostContent, getPostSlugs, PostContent } from '@/lib/posts';
 import hljs from 'highlight.js';
 import Head from 'next/head';
 import { Fragment, useEffect } from 'react';
-import { remark } from 'remark';
-import html from 'remark-html';
+import ReactMarkdown from 'react-markdown';
 
 import 'highlight.js/styles/github-dark-dimmed.css';
 import '../../app/globals.css';
@@ -17,7 +16,6 @@ interface Params {
 
 interface Props {
   post: PostContent;
-  htmlContent: string;
 }
 
 export async function getStaticPaths() {
@@ -31,16 +29,14 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }: Params): Promise<{ props: Props }> {
   const post = await getPostContent(params.slug);
-  const content = await remark().use(html).process(post.content);
   return {
     props: {
-      post,
-      htmlContent: content.toString()
+      post
     }
   };
 }
 
-export default function Post({ post, htmlContent }: Props) {
+export default function Post({ post }: Props) {
   useEffect(() => {
     hljs.highlightAll();
   });
@@ -54,14 +50,15 @@ export default function Post({ post, htmlContent }: Props) {
       <BackBanner link="/blog" text="blog" />
 
       <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
-        <article className="mx-auto w-full max-w-2xl prose prose-sm sm:prose-base lg:prose-lg prose-blue dark:prose-invert">
+        <article className="mx-auto mb-6 w-full max-w-2xl prose prose-sm sm:prose-base lg:prose-lg prose-blue dark:prose-invert">
           <div className="text-center">
             <h1 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
               {post.title}
             </h1>
           </div>
 
-          <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+
+            <ReactMarkdown>{ post.content }</ReactMarkdown>
         </article>
       </div>
     </Fragment>
